67c6c2ca5321a332bd2cd0d044ed454987d1fc5a
wonho <wonho951@gmail.com>
2021-06-21 10:57:30 +0900

    2021.06.21 수업내용

--- src/echo/Server.java
+++ src/echo/Server.java
@@ -15,210 +15,71 @@ public class Server {
 
 	public static void main(String[] args) throws IOException {
 		
-		/*
-		// 소켓 프로그래밍
-		ServerSocket serverSocket = new ServerSocket();	//메모리에 올림
-		serverSocket.bind(new InetSocketAddress("183.96.42.64", 10001));	//IP 포트번호 /바인딩 세팅함.
-		//포트는 항구라고 생각.
+		
+		//스레드 + 소켓통신 (소켓반복생성,다중접속?)
+		
+		ServerSocket serverSocket = new ServerSocket();	
+		serverSocket.bind(new InetSocketAddress("183.96.42.64", 10001));
 		
 		System.out.println("<서버시작>");
 		System.out.println("==============================");
 		System.out.println("연결을 기다리고 있습니다.");
 		
 		
-		Socket socket = serverSocket.accept();	//서버랑 클라이언트랑 서로 연결고리임
-		//클라이언트 누군가 오면 어쎕트()안에 new socket이 만들어져서 socket에 담긴다.
-		
-		
-		System.out.println("클라이언트가 연결 되었습니다.");
-		
-		
-		//메세지 받기용 스트림	(클라이언트에서 서버로 받음)
-		InputStream is = socket.getInputStream();	// 주스트림--> 걍 달라고함.(처음 빨대 꽂고)
-		InputStreamReader isr = new InputStreamReader(is, "UTF-8");	//중간빨대랑 처음 빨대 연결해줌.
-		BufferedReader br = new BufferedReader(isr);	//제일 큰 빨대에 연결해서 공차 먹듯이ㅇㅇ
-		
-		//메세지 보내기용 스트림	(서버에서 클라이언트로)
-		OutputStream os = socket.getOutputStream();
-		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-		BufferedWriter bw = new BufferedWriter(osw);
-				
-				
-				
-		//실제 메세지 받기
-		String msg = br.readLine();
-		System.out.println("받은 메세지:" + msg);
-		
-		
-		
-		
-		
-		//메세지 보내기.
-		bw.write(msg);	//쓰는거지 보내는 의미는 아님.
-		bw.newLine();
-		bw.flush();	//강제로 보냄.
-		
-		
-		
-		
-		//생략가능.
-		//br.close();
-		//bw.close();
-		socket.close();
-		serverSocket.close();// 얘도 닫아줘야함.
-		*/
-		
-		
-		
-		/*
-		//반복문 만들자
-		// 소켓 프로그래밍
-		ServerSocket serverSocket = new ServerSocket();	//메모리에 올림
-		serverSocket.bind(new InetSocketAddress("183.96.42.64", 10001));	//IP 포트번호 /바인딩 세팅함.
-		//포트는 항구라고 생각.
-		
-		System.out.println("<서버시작>");
-		System.out.println("==============================");
-		System.out.println("연결을 기다리고 있습니다.");
-		
-		
-		Socket socket = serverSocket.accept();	//서버랑 클라이언트랑 서로 연결고리임
-		//클라이언트 누군가 오면 어쎕트()안에 new socket이 만들어져서 socket에 담긴다.
-		
-		
-		System.out.println("클라이언트가 연결 되었습니다.");
-		
-		
-		//메세지 받기용 스트림	(클라이언트에서 서버로 받음)
-		InputStream is = socket.getInputStream();	// 주스트림--> 걍 달라고함.(처음 빨대 꽂고)
-		InputStreamReader isr = new InputStreamReader(is, "UTF-8");	//중간빨대랑 처음 빨대 연결해줌.
-		BufferedReader br = new BufferedReader(isr);	//제일 큰 빨대에 연결해서 공차 먹듯이ㅇㅇ
-		
-		//메세지 보내기용 스트림	(서버에서 클라이언트로)
-		OutputStream os = socket.getOutputStream();
-		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-		BufferedWriter bw = new BufferedWriter(osw);
-				
-		
-		while(true) {
+		//*반복구간
+		while(true) {		
+			Socket socket = serverSocket.accept();	
+			System.out.println("클라이언트가 연결 되었습니다.");
 			
-			//실제 메세지 받기
+			//출장 --> 세팅한 후 + 메세지 주고받기.
+			Thread thread = new ServerThread();
+			thread.start();
+		}
+		
+		
+		
+		//메세지 받기용 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		
+		//*Thread run()에서 코딩이 되는애 ***********************
+		//메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+				
+		//메세지 주고받기
+		while(true) {
+			//메세지받기
 			String msg = br.readLine();
 			
 			
-			if(msg == null) {	//null은 주소비교이기 때문에 == 이걸로 함. equals() 안씀.ㅇㅋ?
+			if(msg == null) {	
 				System.out.println("[클라이언트 접속 종료]");
 				break;
 			}
 			
 			System.out.println("받은 메세지:" + msg);
 			
-			//메세지 보내기.
-			bw.write(msg);	//쓰는거지 보내는 의미는 아님.
+			//메세지 보내기
+			bw.write(msg);	
 			bw.newLine();
-			bw.flush();	//강제로 보냄.
+			bw.flush();	
 		}
-				
+		//*Thread run()에서 코딩이 되는애 ***********************
+		
+
+		
 		System.out.println("==============================");
 		System.out.println("<서버종료>");
 		
-		
-		//생략가능.
-		//br.close();
-		//bw.close();
 		socket.close();
-		serverSocket.close();// 얘도 닫아줘야함.
-		*/
-		
-		
-		//	흉내내기
-		// 소켓 프로그래밍
-		ServerSocket serverSocket = new ServerSocket();	//메모리에 올림
-		serverSocket.bind(new InetSocketAddress("183.96.42.64", 10001));	//IP 포트번호 /바인딩 세팅함.
-		//포트는 항구라고 생각.
-		
-		System.out.println("<서버시작>");
-		System.out.println("==============================");
-		System.out.println("연결을 기다리고 있습니다.");
-		
-		
-		Socket socket = serverSocket.accept();	//서버랑 클라이언트랑 서로 연결고리임
-		//클라이언트 누군가 오면 어쎕트()안에 new socket이 만들어져서 socket에 담긴다.
-		
-		
-		System.out.println("클라이언트가 연결 되었습니다.");
-		
-		
-		//메세지 받기용 스트림	(클라이언트에서 서버로 받음)
-		InputStream is = socket.getInputStream();	// 주스트림--> 걍 달라고함.(처음 빨대 꽂고)
-		InputStreamReader isr = new InputStreamReader(is, "UTF-8");	//중간빨대랑 처음 빨대 연결해줌.
-		BufferedReader br = new BufferedReader(isr);	//제일 큰 빨대에 연결해서 공차 먹듯이ㅇㅇ
-		
-		//메세지 보내기용 스트림	(서버에서 클라이언트로)
-		OutputStream os = socket.getOutputStream();
-		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-		BufferedWriter bw = new BufferedWriter(osw);
-				
-		
-		while(true) {
-			
-			//실제 메세지 받기
-			String msg = br.readLine();
-			
-			
-			if(msg == null) {	//null은 주소비교이기 때문에 == 이걸로 함. equals() 안씀.ㅇㅋ?
-				System.out.println("[클라이언트 접속 종료]");
-				break;
-			}
-			
-			System.out.println("받은 메세지:" + msg);
-			
-			//메세지 보내기.
-			bw.write(msg);	//쓰는거지 보내는 의미는 아님.
-			bw.newLine();
-			bw.flush();	//강제로 보냄.
-		}
-				
-		System.out.println("==============================");
-		System.out.println("<서버종료>");
-		
-		
-		//생략가능.
-		//br.close();
-		//bw.close();
-		socket.close();
-		serverSocket.close();// 얘도 닫아줘야함.
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		serverSocket.close();
 		
 		
 
-		/*
-		 * ServerSocket serverSocket = new ServerSocket(); serverSocket.(new
-		 * InetSocketAddress("192.168.0.133", 10001)); // IP 포트번호
-		 * 
-		 * System.out.println("<서버시작>");
-		 * System.out.println("================================");
-		 * System.out.println("[연결을 기다리고 있습니다.]");
-		 * 
-		 * Socket socket = serverSocket.accept(); // 클라이언트 누군가 오면 어쎕트()안에 new socket이
-		 * 만들어져서 socket에 담긴다.
-		 * 
-		 * System.out.println("클라이언트가 연결 되었습니다.");
-		 * 
-		 * 
-		 * socket.close();
-		 * serverSocket.close();//얘도 닫아줘야함.
-		 */
 	}
 
 }
---
+++ src/echo/ServerThread.java
@@ -0,0 +1,20 @@
+package echo;
+
+public class ServerThread extends Thread{//출장보내는거.
+	//출장 관련(독립적인 프로세서) -->부모(Thread)쪽에 구현되어 있다.
+	
+	//출장가서 해야 할 일	--> 메세지 주고받기.
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+	
+
+	
+	
+	
+	
+	
+	
+}
